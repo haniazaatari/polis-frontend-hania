75358a015b6c690145ee566cc359f5682edf083e
Hania Zaatari <haniazaatari@gmail.com>
2025-12-19 00:53:04 +0200

    enabled pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  public: {
+    polisApi: 'http://localhost:5000'
+  },
 
     devServer: {
     port: 6002
   },
+   pages: true,
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
