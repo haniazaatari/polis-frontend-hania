f2c6d7bd1d0af9736fe1fb32b5f2fcb2c8f81af2
Hania Zaatari <haniazaatari@gmail.com>
2025-12-19 00:49:00 +0200

    updated port

--- composables/usePolis.ts
+++ composables/usePolis.ts
@@ -1,12 +1,31 @@
 export function usePolis() {
-  const baseURL = 'http://localhost';
+  const baseURL = 'http://localhost:5000';
 
   async function getConversation(id: string) {
-    return await $fetch(`/api/v3/conversations/${id}`, { baseURL });
+    return await $fetch(`/api/v3/conversations/${id}`, { baseURL, credentials: 'include' });
   }
 
   async function getComments(conversationId: string) {
-    return await $fetch(`/api/v3/comments?conversation_id=${conversationId}`, { baseURL });
+    return await $fetch(`/api/v3/comments?conversation_id=${conversationId}`, {
+      baseURL,
+      credentials: 'include'
+    });
+  }
+
+  return { getConversation, getComments };
+}
+export function usePolis() {
+  const baseURL = 'http://localhost:5000';
+
+  async function getConversation(id: string) {
+    return await $fetch(`/api/v3/conversations/${id}`, { baseURL, credentials: 'include' });
+  }
+
+  async function getComments(conversationId: string) {
+    return await $fetch(`/api/v3/comments?conversation_id=${conversationId}`, {
+      baseURL,
+      credentials: 'include'
+    });
   }
 
   return { getConversation, getComments };
